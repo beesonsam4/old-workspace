158a8307c39f7b31b3db8682f4db82391f5896e9
beesonsam4 <beesonsam4@gmail.com>
2023-12-02 00:06:40 -0500

    theme change

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	//...
+	daisyui: {
+	  themes: ["light", "dark", "cupcake"],
+	},
+  }
+module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
@@ -9,5 +15,6 @@ module.exports = {
 		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
 		darkTheme: "dark", // name of one of the included themes for dark mode
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
+
 	  }
 }
